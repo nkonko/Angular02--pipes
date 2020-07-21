f9f735959843c0db17cf0d3c93ad3ba9670100ec
Nicolas Omar Azzara <nicoazzara@hotmail.com>
2020-07-21 10:57:05 -0300

    Angular Pipes proyect
    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+name:string = 'Capitan America';
+
+customPipeValue = "hola hola hola";
+
+array = [1,2,3,4,5,6];
+
+PI: number = Math.PI;
+
+percent: number = 0.234;
+
+salary:number = 1234.5;
+
+date: Date = new Date(1999,0,1);
+
+videoUrl:string = "https://www.youtube.com/embed/xpkcnnaA5IE";
+
+promiseValue = new Promise<string>((resolve) =>
+{
+  setTimeout(() => {
+    resolve('finished');
+  },4500);
+});
+
+hero = {
+  name:'Logan',
+  password: 'abc',
+  years: 33,
+  Address:
+  {
+    street: 'Rincon',
+    numero: 123
+  }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
+import { SanitizeUrlPipe } from './pipes/sanitize-url.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePipe,
+    SanitizeUrlPipe
   ],
   imports: [
     BrowserModule
